e9abec62de7ff6c77bae5558656a22f1a9307cf0
zero0ut <alex.kpc@gmail.com>
2019-10-05 10:24:42 +0800

    Format the Pet page

--- 02_adopt-pets/src/views/Pet.vue
+++ 02_adopt-pets/src/views/Pet.vue
@@ -1,6 +1,35 @@
 <template>
     <div>
-        {{ $route.params.species }}
-        {{ $route.params.id }}
+        <h1>{{ animal.name }} ({{ this.$route.params.species }}) </h1>
+        <p>Age: {{ animal.age }} years old</p>
+        <p>Breed: {{ animal.breed }}</p>
     </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    // cats() {
+    //     return this.$store.state.cats
+    // },
+    // dogs() {
+    //     return this.$store.state.cats
+    // }
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted() {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
